7c4c0ef5bbbbd622cbac56761a73104d23e3c009
Leon Mil <leon.mil@gmail.com>
2020-01-09 21:07:35 -0500

    Filtering a list

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -12,7 +12,22 @@ export class ProductListComponent implements OnInit {
     imageWidth = 50;
     imageMargin = 2;
     showImage = false;
-    listFilter = 'cart';
+
+    _listFilter: string;
+    get listFilter(): string {
+      return this._listFilter;
+    }
+    set listFilter(value: string) {
+      this._listFilter = value;
+      this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    constructor() {
+      this.filteredProducts = this.products;
+      this.listFilter = 'cart';
+    }
+
+    filteredProducts: IProduct[];
     products: IProduct[] =
     [
         {
@@ -66,6 +81,12 @@ export class ProductListComponent implements OnInit {
           imageUrl: 'assets/images/xbox-controller.png'
         }
     ];
+
+    performFilter(filterBy: string): IProduct[] {
+      filterBy = filterBy.toLocaleLowerCase();
+      return this.products.filter((product: IProduct) =>
+        product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
