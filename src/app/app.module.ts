5d709ed8136bf816848664ac6137280371ec7f2a
Leon Mil (CENSUS/EAD FED) <leon.mil@census.gov>
2020-01-10 15:56:43 -0500

    Renaming star component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/StarComponent';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
